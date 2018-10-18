f0883b87a14e941bc5d474392965886c1d175191
Richard Mori <r.mori@meshpower.co.rw>
2018-10-17 19:08:19 -0600

    Initial commit

---
+++ build.py
@@ -0,0 +1,30 @@
+from staticjinja import Site
+import sys
+
+import json
+def load_json(file):
+    def wrap():
+        f = open(file, 'r')
+        try:
+            d = json.loads(f.read())
+        except Exception as e:
+            print("unable to load %s" % file)
+            print(e)
+            sys.exit(0)
+        f.close()
+        return d
+    return wrap
+
+if __name__ == "__main__":
+    site = Site.make_site(contexts=[
+        ('.*.html', load_json('content/base.json')),
+        ('index.html', load_json('content/testimonials.json')),
+        ('index.html', load_json('content/index.json')),
+        ('how-it-works.html', load_json('content/testimonials.json')),
+        ('how-it-works.html', load_json('content/how-it-works.json')),
+        ('impact.html', load_json('content/impact.json')),
+        ('contact.html', load_json('content/contact.json')),
+        # ('about.html', load_json('content/about.json'))
+    ], mergecontexts=True, outpath='site')
+
+    site.render(use_reloader=True)
